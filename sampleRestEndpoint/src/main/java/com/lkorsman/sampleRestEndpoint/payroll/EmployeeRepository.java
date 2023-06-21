c03f3414e1b803aa715a7b2bf18f99afcd1c2f3a
lkorsman <lukekorsman@gmail.com>
2023-06-21 13:35:06 -0400

    Initial Commit
    Tutorial based off of
    https://spring.io/guides/tutorials/rest/

---
+++ sampleRestEndpoint/src/main/java/com/lkorsman/sampleRestEndpoint/payroll/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.lkorsman.sampleRestEndpoint.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ sampleRestEndpoint/src/main/java/com/lkorsman/sampleRestEndpoint/payroll/SampleRestEndpointApplication.java
@@ -0,0 +1,14 @@
+package com.lkorsman.sampleRestEndpoint.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestEndpointApplication.class, args);
+
+	}
+
+}
